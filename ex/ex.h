89f538e9d6ed9ab1ff838b6e9d5a5695204ef46b
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2024-12-28 14:38:06 +0100

    restrict size of _excmd.agv_flags to 4 bits
    Four bits are sufficient for the flags assigned to this field and
    using only that many makes room for another bitfield in the next commit.
    
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

--- ex/ex.h
+++ ex/ex.h
@@ -98,7 +98,7 @@ struct _excmd {
 #define	AGV_GLOBAL	0x04		/* global command. */
 #define	AGV_V		0x08		/* v command. */
 #define	AGV_ALL		(AGV_AT | AGV_AT_NORANGE | AGV_GLOBAL | AGV_V)
-	u_int8_t  agv_flags;
+	u_int8_t  agv_flags : 4;
 
 	/* Clear the structure before each ex command. */
 #define	CLEAR_EX_CMD(cmdp) {						\
