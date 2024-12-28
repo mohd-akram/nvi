957906b0e3bcc0f795cf8c6b7313fcc4ec338784
Sven Verdoolaege <sven.verdoolaege@gmail.com>
2024-12-28 15:09:35 +0100

    ex: properly global insert with initial newline in inserted text
    The line insertion was guarded by a test that would fail
    to trigger on empty lines in the inserted text.
    It is not clear why this guard appears in the code.
    Removing the guard reportedly makes the code behave
    in the same way as the original vi.
    The version of vi that comes with AIX and that identifies itself
    as "Version 3.10" also behaves in this way.
    
    Reported-by: Mohamed Akram <mohd.akram@outlook.com>
    Patch-by: Mohamed Akram <mohd.akram@outlook.com>
    Signed-off-by: Sven Verdoolaege <sven.verdoolaege@gmail.com>

---
+++ tests/ex/global_insert_empty_line2.ex
@@ -0,0 +1,8 @@
+" Global insert with delimiter and newline.
+a
+test
+.
+g/test/i|\
+.
+w
+q
---
+++ tests/ex/global_insert_nothing2.ex
@@ -0,0 +1,8 @@
+" Global insert with non-trailing newline that does not insert anything.
+a
+test
+.
+g/test/i\
+.
+w
+q
